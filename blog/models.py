a65ea9f28ee5e07abd993d0674a31218699ea024
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2024-11-28 01:03:30 +0500

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    age = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# class Book(models.Model):
+#     name = models.CharField(max_length=100)
+#     author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+#     genre = models.ForeignKey(BookGenre, null=True, on_delete=models.SET_NULL)
+#
+#     def __str__(self):
+#         return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique=True, blank=True, null=True)
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-    html = f"""
-        <h1>Home Page</h1>
-    """
-    return HttpResponse(html)
+    posts = Post.objects.all()
+    post_list = ""
+    for post in posts:
+        post_list += f"<li>{post.title}</li>"
+    return HttpResponse(f"<ul>{post_list}</ul>")
+
