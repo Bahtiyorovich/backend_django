d4d879e6be6a71ceab53145e69fc61c5d1e78fc4
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2024-11-30 22:58:21 +0500

    form

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import orm_list
+from .views import index_page, get_max_salary_employess, get_dependents, register
 
 urlpatterns = [
-    path('orm-list/', orm_list, name='orm_list'),
+    path('', index_page, name='index_page'),
+    path('max-salary/<int:top>', get_max_salary_employess, name='max-salary-employees'),
+    path('deps/<int:employee_id>', get_dependents, name='deps-list'),
+    path('register/', register, name='register'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,25 @@
-from itertools import count
-
 from django.shortcuts import render
-from .models import Countries, Employees
-from django.db.models import Q
-# Create your views here.
-from django.http import HttpResponse
+# from itertools import count
+from .models import Countries, Employees, Dependents
+# from django.db.models import Q
+# # Create your views here.
+# from django.http import HttpResponse
+
+def index_page(request):
+    return render(request, "index.html")
+
+def get_max_salary_employess(request, top):
+    queryset = Employees.objects.all().order_by('-salary')[:top]
+    return render(request, 'max_salary.html', {"max_salary": queryset})
+
+def get_dependents(request, employee_id):
+    queryset = Dependents.objects.all().filter(employee=employee_id)
+    employee = Employees.objects.get(employee_id=employee_id)
+    context = {'employee': employee, 'deps': queryset}
+    return render(request, 'dependents.html', context)
+
+def register(request):
+    return render(request, 'register.html')
 
 # def orm_list(request):
     # queryset = Countries.objects.all()
@@ -20,13 +35,21 @@ from django.http import HttpResponse
     #     res += f"<li>{query.country_name}</li>"
     # return HttpResponse(f"<ul>{res}</ul>")
 
-def orm_list(request):
-    employees = Employees.objects.filter(Q(first_name__startswith="A")|
-                                         Q(last_name__startswith="B"))
-    # employees = Employees.objects.values('first_name', 'last_name')
-    print(employees.query)
-    employe = ""
-    for emp in employees:
-        # employe += f"{emp['first_name']} {emp['last_name']} <br>"
-        employe += f"{emp.first_name} {emp.last_name} <br>"
-    return HttpResponse(employe)
+# def orm_list(request):
+#     employees = Employees.objects.filter(Q(first_name__startswith="A")|
+#                                          Q(last_name__startswith="B"))
+#     # employees = Employees.objects.values('first_name', 'last_name')
+#     print(employees.query)
+#     employe = ""
+#     for emp in employees:
+#         # employe += f"{emp['first_name']} {emp['last_name']} <br>"
+#         employe += f"{emp.first_name} {emp.last_name} <br>"
+#     return HttpResponse(employe)
+
+
+
+
+
+
+
+
